ad08f835b5b3deaf3914c7897320c84dcd31f235
Matthew Kozirev <matthew.kozirev@gmail.com>
2021-10-08 17:12:27 +1030

    Added basic role add/remove commands that require mmanage role permissions.

---
+++ AuTan/Modules/RoleModule.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace AuTan.Modules
+{
+    [Group("role")]
+    public class RoleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("add")]
+        [Summary("Add a role(s)")]
+        public async Task AddRollAsyync(params IRole[] roles)
+        {
+            IGuildUser user = Context.User as IGuildUser;
+            if (user.GuildPermissions.ManageRoles)
+            {
+                ulong[] role_ids = new ulong[roles.Length];
+                for (int i = 0; i < roles.Length; i++)
+                {
+                    role_ids[i] = roles[i].Id;
+                }
+                await user.AddRolesAsync(role_ids);
+            }
+        }
+
+        [Command("remove")]
+        [Summary("Remove a role(s)")]
+        public async Task RemoveRollAsyync(params IRole[] roles)
+        {
+            IGuildUser user = Context.User as IGuildUser;
+            if (user.GuildPermissions.ManageRoles)
+            {
+                ulong[] role_ids = new ulong[roles.Length];
+                for (int i = 0; i < roles.Length; i++)
+                { 
+            
+                    role_ids[i] = roles[i].Id;
+                }
+                await user.RemoveRolesAsync(role_ids);
+            }
+        }
+    }
+}
